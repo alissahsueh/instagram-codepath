f80a1870af084fea043ac8214b2b3562067d6933
alissahsueh <alissahsueh0313@berkeley.edu>
2019-07-08 21:28:32 -0700

    initial commit

---
+++ app/src/main/java/com/example/instagram/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.instagram;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText usernameInput;
+    private EditText passwordInput;
+    private Button loginBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        usernameInput = findViewById(R.id.etUserName);
+        passwordInput = findViewById(R.id.etPassWord);
+        loginBtn = findViewById(R.id.btnLogin);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String username = usernameInput.getText().toString();
+                final String password = passwordInput.getText().toString();
+
+                //pass these inputs into the login method
+                login(username, password);
+            }
+        });
+
+    }
+
+    private void login(String username, String password) {
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if (e == null) {
+                    Log.d("LoginActivity", "Login successful!");
+                    final Intent intent = new Intent(MainActivity.this, HomeActivity.class );
+                    startActivity(intent);
+                    //this is so that the user cannot just back up and see password and username
+                    finish();
+
+                } else {
+                    Log.e("LoginActivity", "Login failure");
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/instagram/ParseApp.java
@@ -0,0 +1,24 @@
+package com.example.instagram;
+
+import android.app.Application;
+
+import com.example.instagram.model.Post;
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+public class ParseApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        ParseObject.registerSubclass(Post.class);
+        final Parse.Configuration configuration = new Parse.Configuration.Builder(this)
+                .applicationId("fbu_insta")
+                .clientKey("alissa_testing")
+                .server("http://alissahsueh-fbu-instagram.herokuapp.com/parse")
+                .build();
+
+        Parse.initialize(configuration);
+    }
+
+}
